a7ae7f587fb65d49eb3ca0e09319d0b83b8c26aa
aistepnews <ai.stepnews@gmail.com>
2025-04-27 15:23:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# إعدادات السماح للاتصال من موقع GitHub Pages
+origins = [
+    "https://aistepnews.github.io",  # رابط الواجهة الأمامية تبعك
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
