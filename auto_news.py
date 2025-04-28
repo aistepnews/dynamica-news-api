fa4829853a35c3aabbdd8be7be2d830800d76557
aistepnews <ai.stepnews@gmail.com>
2025-04-28 13:55:33 +0300

    Update auto_news.py

--- auto_news.py
+++ auto_news.py
@@ -1,7 +1,7 @@
 import requests
 
 def auto_fetch_news():
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY"
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=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"
     response = requests.get(url)
     data = response.json()
     return data['articles']
